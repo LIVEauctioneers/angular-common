f0f36be7e756ce50061acbbae9b58e19f50d2aa8
Rui Fang Li <maze.sunlighted@gmail.com>
2016-04-27 13:11:18 -0600

    add lint task

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var plugins = require('gulp-load-plugins')({pattern: '*'});
 var config = require('./build.config.js')();
 var clangFormatSettings = require('./clang-format.json'); // TODO: it wasn't reading.clang-format for some reason. confirm default file name
 
+var JS_PATTERN = '/**/*.js';
+
 gulp.task('clean', function(){
     return plugins.del([config.output]);
 });
@@ -14,7 +16,7 @@ gulp.task('build', ['lint'], function(){
     // perform build for each module
     var src = config.src;
     var tasks = getFolders(src).map(function(directory){
-        return gulp.src(path.join(src, directory, '/**/*.js'))
+        return gulp.src(path.join(src, directory, JS_PATTERN))
             .pipe(plugins.concat(directory + '.js'))
             .pipe(gulp.dest(config.output));
     });
@@ -28,11 +30,12 @@ gulp.task('build', ['lint'], function(){
 });
 
 gulp.task('lint', function(){
-
+    return gulp.src(path.join(config.src, JS_PATTERN))
+        .pipe(plugins.clangFormat.checkFormat(clangFormatSettings, undefined, {verbose: true, fail: true}));
 });
 
 gulp.task('format', function(){
-    return gulp.src(path.join(config.src, '/**/*.js'))
+    return gulp.src(path.join(config.src, JS_PATTERN))
         .pipe(plugins.clangFormat.format(clangFormatSettings))
         .on('warning', function(e) {
            process.stdout.write(e.message);
